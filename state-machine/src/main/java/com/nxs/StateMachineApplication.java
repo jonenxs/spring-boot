c579edc456b153f2189444e4cbba80f6ac055818
jonenxs <570141370@qq.com>
2018-01-03 23:47:41 +0800

    使用Spring StateMachine框架实现状态机

--- state-machine/src/main/java/com/nxs/StateMachineApplication.java
+++ state-machine/src/main/java/com/nxs/StateMachineApplication.java
@@ -1,12 +1,28 @@
 package com.nxs;
 
+import com.nxs.enums.Events;
+import com.nxs.enums.States;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.listener.StateMachineListener;
 
 @SpringBootApplication
-public class StateMachineApplication {
+public class StateMachineApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StateMachineApplication.class, args);
+	}
+
+	@Autowired
+	private StateMachine<States, Events> stateMachine;
+
+	@Override
+	public void run(String... strings) throws Exception {
+		stateMachine.start();
+		stateMachine.sendEvent(Events.PAY);
+		stateMachine.sendEvent(Events.RECEIVE);
 	}
 }
